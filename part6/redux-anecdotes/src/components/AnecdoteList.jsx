c443ae7eec0bbfb4b90f965956b95e17bdda2493
Furkan Dursun <furkandursun3428@gmail.com>
2024-12-16 17:41:49 +0300

    Exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,15 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if (state.filter === true) {
+      return state.anecdotes
+    } else {
+      return state.anecdotes
+        .filter(anecdote => anecdote.content.match(new RegExp(state.filter, 'i')))
+    }
+  })
+  
   anecdotes.sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
