93c6a99162c57ff643dcf8cb5972d5c9c2921b81
Furkan Dursun <furkandursun3428@gmail.com>
2025-02-03 16:29:48 +0300

    Exercises 9.8-9.13

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,29 @@
+import patients from "../../data/patients";
+import { NewPatient, NonSensitivePatient, Patient } from "../types";
+import { v1 as uuid } from "uuid";
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...patient,
+  };
+
+  patients.push(newPatient);
+  return newPatient;
+};
+
+export default { getPatients, getNonSensitivePatients, addPatient };
